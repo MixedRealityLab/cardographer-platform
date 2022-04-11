39c65c540b9f2df9d6e620d326681e7ce6d4af67
Kevin Glover <kevin.glover@nottingham.ac.uk>
2022-04-11 14:42:31 +0100

    Update snapshot.ts

--- src/routes/api/user/sessions/[sessionId]/snapshot.ts
+++ src/routes/api/user/sessions/[sessionId]/snapshot.ts
@@ -34,6 +34,7 @@ export const put: RequestHandler = async function ({locals, params, request}) {
 	const db = await getDb();
 	// permission check
 	let session: Session
+	console.log(sessionId)
 	if (sessionId === 'new') {
 		session = {
 			_id: getNewId(),
@@ -54,6 +55,7 @@ export const put: RequestHandler = async function ({locals, params, request}) {
 			_id: sessionId, owners: locals.email
 		})
 	}
+	console.log(session)
 	if (!session) {
 		return {status: 404};
 	}
