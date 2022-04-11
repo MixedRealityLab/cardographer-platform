4d5c31fdd4039efc41e49f60c389da0fea9fe3e3
Kevin Glover <kevin.glover@nottingham.ac.uk>
2022-04-11 14:35:27 +0100

    Update sidebar.svelte

--- src/routes/user/miro/sidebar.svelte
+++ src/routes/user/miro/sidebar.svelte
@@ -320,10 +320,10 @@
 				<div class="flex-1 flex flex-col">
 					<div class="flex-1">
 						{#each widgets as widget (widget.id)}
-							<div class="flex py-2 items-center cursor-pointer transition-opacity duration-300 hover:opacity-50"
+							<div class="flex py-2 items-center cursor-pointer transition-opacity duration-300 hover:opacity-80"
 							     on:click={() => selectWidget(widget)}>
-								<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
-								     stroke="currentColor" stroke-width="2">
+								<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-4" fill="none" viewBox="0 0 24 24"
+								     stroke="#fde047" stroke-width="2">
 									<path stroke-linecap="round" stroke-linejoin="round"
 									      d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"/>
 								</svg>
