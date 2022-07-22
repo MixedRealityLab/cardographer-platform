b2669bf8a2f65b1dbb334e8447b95469ea3a9bf6
Kevin Glover <kevin.glover@nottingham.ac.uk>
2022-07-22 12:35:49 +0100

    Sigh

--- src/routes/user/miro/index.svelte
+++ src/routes/user/miro/index.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import {base} from '$app/paths'
 	import {Miro} from "$lib/miro";
+	import {onMount} from "svelte";
 
 	declare const miro: Miro
 	const {board} = miro;
@@ -14,5 +15,7 @@
 		});
 	}
 
-	init();
+	onMount(async () => {
+		await init();
+	})
 </script>
