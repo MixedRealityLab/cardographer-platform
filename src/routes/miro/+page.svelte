f93430dd8bd9cdcb4c92c512750ee768cea5ea91
Kevin Glover <kevin.glover@nottingham.ac.uk>
2023-01-19 11:53:03 +0000

    Update +page.svelte

--- src/routes/miro/+page.svelte
+++ src/routes/miro/+page.svelte
@@ -4,7 +4,10 @@
 
 	declare const miro: Miro
 
+	console.log("Wibble")
+
 	async function init() {
+		console.log("Init")
 		miro.board.ui.on("icon:click", async () => {
 			const info = await miro.board.getInfo()
 			await miro.board.ui.openPanel({
