ba6a32a9aa641310cc708a9c19938ad1e0f61a5b
Kevin Glover <kevin.glover@nottingham.ac.uk>
2023-01-25 13:50:26 +0000

    Improvements

--- src/lib/clients/miro.ts
+++ src/lib/clients/miro.ts
@@ -132,6 +132,7 @@ export class MiroClient extends Client {
 			}
 			if (debug) console.log(`card ${id} in ${ci.zones.length} zones: ${JSON.stringify(ci.zones)}`);
 		}
+		if (debug) console.log(boards)
 		return {boards}
 	}
 }
