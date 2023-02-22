e1a6955e18767b76f76f9df2437b9a59513591b4
Kevin Glover <kevin.glover@nottingham.ac.uk>
2023-02-22 13:11:10 +0000

    Fix image urls...

--- src/routes/sessions/[sessionId]/cards/+page.svelte
+++ src/routes/sessions/[sessionId]/cards/+page.svelte
@@ -1,18 +1,23 @@
 <script lang="ts">
+	import {base} from "$app/paths";
+
 	export let data
+
+	let cardList: HTMLElement
 
 	function handleScrollClick(event: MouseEvent) {
 		console.log(event)
 		event.clientX / event.target.clientWidth
+
 	}
 </script>
 
 {#if data.decks}
-	<div class="snap-x snap-mandatory flex overflow-x-scroll overflow-y-hidden bg-gray-200"
-	     on:click={handleScrollClick}>
+	<ol class="snap-x snap-mandatory flex overflow-x-scroll overflow-y-hidden bg-gray-200"
+	    on:click={handleScrollClick} bind:this={cardList}>
 		{#each data.decks as deck}
 			{#each deck.cards as card}
-				<div class="snap-center">
+				<li class="snap-center">
 					<div class="w-screen h-screen relative" style="height: 100svh">
 						<div class="inset-8 absolute rounded-3xl bg-white p-6 overflow-clip flex flex-col justify-center drop-shadow">
 							<div class="text-2xl">{card.name}</div>
@@ -20,11 +25,11 @@
 						</div>
 						{#if card.frontUrl}
 							<div class="inset-8 absolute rounded-3xl bg-origin-content bg-center bg-contain bg-white bg-no-repeat"
-							     style="background-image: url({card.frontUrl})"></div>
+							     style="background-image: url({card.frontUrl.startsWith('/') ? base + card.frontUrl : card.frontUrl})"></div>
 						{/if}
 					</div>
-				</div>
+				</li>
 			{/each}
 		{/each}
-	</div>
+	</ol>
 {/if}
