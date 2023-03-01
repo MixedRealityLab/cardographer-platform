4168a37233ededf6c85767cc302cb8b08fb1002b
Kevin Glover <kevin.glover@nottingham.ac.uk>
2023-03-01 11:16:11 +0000

    Fix menu

--- src/routes/sessions/[sessionId]/cards/+page.svelte
+++ src/routes/sessions/[sessionId]/cards/+page.svelte
@@ -20,42 +20,37 @@
 <div class="flex flex-col h-screen w-screen" style="height: 100svh">
 	{#if data.decks}
 		<CardList cards={cards}></CardList>
-		<div class="flex p-3 gap-2">
+		<div class="flex p-3 gap-2 relative">
 			<input class="flex-1" type="text" placeholder="Search" bind:value={search}/>
 			{#if categories.length > 1}
-				<!--				<button class="button" on:click={() => menu.setOpen(true)}-->
-				<!--				        style="height: 2.5rem; width: 2.5rem; line-height: inherit; padding: 0.25rem; min-width: inherit">-->
-				<!--				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960">-->
-				<!--					<path d="M400 816v-60h160v60H400ZM240 606v-60h480v60H240ZM120 396v-60h720v60H120Z"/>-->
-				<!--				</svg>-->
-				<!--				</button>-->
-				<Menu class="relative">
+				<Menu>
 					<MenuButton class="button button-slim h-10 w-10">
 						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960">
 							<path d="M400 816v-60h160v60H400ZM240 606v-60h480v60H240ZM120 396v-60h720v60H120Z"/>
 						</svg>
 					</MenuButton>
 					<MenuItems
-							class="absolute bottom-0 right-0 bg-white rounded drop-shadow p-1 flex flex-col items-stretch">
+							class="absolute bottom-14 right-3 bg-white rounded drop-shadow p-1 flex flex-col items-stretch"
+							style="max-width: 70svw">
 						<MenuItem let:active on:click={() => currentCategory=null}>
 							<div class="flex items-center transition-colors duration-300 rounded px-2 py-1 cursor-pointer hover:bg-blue-200"
 							     class:bg-blue-300={!currentCategory}>
-								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" class="w-4 h-4 mr-2"
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" class="w-5 h-5 mr-2"
 								     class:opacity-0={currentCategory}>
 									<path d="M378 810 154 586l43-43 181 181 384-384 43 43-427 427Z"/>
 								</svg>
-								All
+								<span class="flex-1">All</span>
 							</div>
 						</MenuItem>
 						{#each categories as category}
 							<MenuItem let:active on:click={() => currentCategory=category}>
 								<div class="flex items-center transition-colors duration-300 rounded px-2 py-1 cursor-pointer hover:bg-blue-200"
 								     class:bg-blue-300={currentCategory === category} class:border-blue-400={active}>
-									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" class="w-4 h-4 mr-2"
+									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" class="w-5 h-5 mr-2"
 									     class:opacity-0={currentCategory !== category}>
 										<path d="M378 810 154 586l43-43 181 181 384-384 43 43-427 427Z"/>
 									</svg>
-									{category}
+									<span class="flex-1">{category}</span>
 								</div>
 							</MenuItem>
 						{/each}
